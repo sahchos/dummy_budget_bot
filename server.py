516c930b21cd06c75dac940a8590884f55b879cd
Alex Suvorov <sahchos93@gmail.com>
2020-05-25 20:40:54 +0300

    Add basic docker configs, simple echo handler

---
+++ server.py
@@ -0,0 +1,22 @@
+import os
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = os.environ.get('API_TOKEN')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
